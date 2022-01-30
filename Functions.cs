a4d39adca84c7a059b366fae5702b4a2e4be8620
Govert van Drimmelen <govert@dnakode.com>
2022-01-30 11:45:14 +0200

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace UsingSQLite
+{
+    public static class MyFunctions
+    {
+        static SqliteConnection _connection;
+        static SqliteCommand _productNameCommand;
+
+        private static void EnsureConnection()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqliteConnection(@"Data Source=C:\Work\Excel-DNA\Samples\UsingSQLiteMDS\Northwind.db");
+                _connection.Open();
+
+                _productNameCommand = new SqliteCommand("SELECT ProductName FROM Products WHERE ProductID = @ProductID", _connection);
+                _productNameCommand.Parameters.Add("@ProductID", SqliteType.Integer);
+            }
+        }
+
+        public static object ProductName(int productID)
+        {
+            try
+            {
+                EnsureConnection();
+                _productNameCommand.Parameters["@ProductID"].Value = productID;
+                return _productNameCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+    }
+}
